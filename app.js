1c0c8bb076d89554b72e49d8a721a5c3a93b64fc
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2023-08-31 16:27:19 -0400

    selecting a puppy leads to hash routed change in html, select puppy page has hash-route to player list

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
 const players = document.getElementById('players');
+const selected = document.getElementById('select');
 
-const state = {
-    players: {},
-    selectPlayer: null
-};
+let playerList = {};
 
 async function getPlayers() {
     const playersResponse = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2307-ftb-et-web-ft/players');
     const playersJson = await playersResponse.json();
-    state.players = playersJson.data.players;
+    playerList = playersJson.data.players;
     renderPlayers();
 }
 
 function renderPlayers() {
-    const allPlayers = state.players.map((player) => {
+    const hash = parseInt(window.location.hash.slice(1));
+    const allPlayers = playerList.map((player) => {
         return `
-        <a href='#${player.id}'>${player.name}</a>
-    `
+            <a href='#${player.id}'>
+            <p>${player.name}</p>
+            <p>${player.breed}</p>
+            </a>
+        `
     }).join('');
     players.innerHTML = allPlayers;
+
+    const selectPlayer = playerList.find(player => player.id === hash);
+
+    if (selectPlayer) {
+        let selectHtml = `
+        <a href=#>go back</a>
+        <div>
+        <p>${selectPlayer.name}</p>
+        <p>${selectPlayer.id}</p>
+        <p>${selectPlayer.breed}</p>
+        <p>${selectPlayer.status}</p>
+        <img src='${selectPlayer.imageUrl}'/>
+        </div>
+        `
+        selected.innerHTML = selectHtml;
+        players.innerHTML = '';
+    } else {
+        selected.innerHTML = 'Choose a player for more information';
+    }
 }
 
 window.addEventListener('hashchange', () => {
-    const hash = parseInt(window.location.hash.slice(1));
-    state.selectPlayer = state.players.find(player => player.id === hash);
-    if (state.selectPlayer) {
-        getSelectPlayer();
-    }
+    renderPlayers();
 });
 
-async function getSelectPlayer() {
-    const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2307-ftb-et-web-ft/players/${state.selectPlayer.id}`)
-    const json = await response.json();
-    state.selectPlayer = json.data.player;
-    console.log(state.selectPlayer)
-}
 
 getPlayers();
