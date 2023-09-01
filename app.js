27b08172870ce91b3e238f6368c923267c4161c8
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2023-09-01 13:07:12 -0400

    second fetch for detailed individual player data and updated hash-route rendering

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const display = document.querySelector('.display');
 const intro = document.getElementById('intro');
 
-let playerList = {};
+const state = {
+    playerList: [],
+    player: null,
+};
 
-async function getPlayers() {
+async function getPlayerList() {
     const playersResponse = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2307-ftb-et-web-ft/players');
     const playersJson = await playersResponse.json();
-    playerList = playersJson.data.players;
-    renderPlayers();
+    state.playerList = playersJson.data.players;
+    renderPlayerList();
 }
 
-function renderPlayers() {
-    const hash = parseInt(window.location.hash.slice(1));
-    const allPlayers = playerList.map((player) => {
+function renderPlayerList() {
+    const allPlayers = state.playerList.map((player) => {
         return `
             <a href='#${player.id}'>
             <h2>${player.name}</h2>
@@ -22,27 +24,45 @@ function renderPlayers() {
     }).join('');
     display.innerHTML = allPlayers;
     intro.innerHTML = 'Click A Puppy For More Information:';
-
-    const selectPlayer = playerList.find(player => player.id === hash);
-
-    if (selectPlayer) {
-        let selectHtml = `
-        <a id="back" href=#> < All Puppies</a>
-        <div id=selectContainer>
-        <h1>${selectPlayer.name}</h1>
-        <p>Breed: ${selectPlayer.breed}</p>
-        <h2>Status: Currently on the ${selectPlayer.status}</h2>
-        <img src='${selectPlayer.imageUrl}'/>
-        </div>
-        `
-        display.innerHTML = selectHtml;
-        intro.innerHTML = '';
-    }
 }
 
 window.addEventListener('hashchange', () => {
-    renderPlayers();
+    render();
 });
 
+function getHash() {
+    const hash = parseInt(window.location.hash.slice(1));
+    state.player = state.playerList.find(player => player.id === hash);
+}
 
-getPlayers();
+async function renderPlayer() {
+    const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2307-ftb-et-web-ft/players/${state.player.id}`);
+    const json = await response.json();
+    state.player = json.data.player;
+
+    let selectHtml = `
+        <a id="back" href=#> < All Puppies</a>
+        <div id=selectContainer>
+        <h1>${state.player.name}</h1>
+        <h2>Team: ${state.player.team.name}</h2>
+        <p>Breed: ${state.player.breed}</p>
+        <h3>Status: Currently on the ${state.player.status}</h3>
+        <img src='${state.player.imageUrl}'/>
+        </div>
+        `
+    display.innerHTML = selectHtml;
+    intro.innerHTML = '';
+    console.log(state.player)
+}
+
+async function render() {
+    await getPlayerList();
+    getHash();
+    if (state.player) {
+        renderPlayer();
+    } else {
+        renderPlayerList();
+    }
+}
+
+render();
